9bd48fac0185569deb693cc6b41e2f63b1a2414d
unforgivenii147 <adnanonagh@gmail.com>
2026-06-20 13:23:53 +0430

    Auto-commit at 2026-06-20 13:23:53

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="json_prettifier",
+    version="1.4.7",
+    author="Isaac Onsgh",
+    author_email="adnsnonagh@gmsil.com",
+    description="A fast JSON prettifier and minifier with multiprocessing support",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/unforgivenii147/json_prettifier",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    entry_points={
+        "console_scripts": [
+            "jb=json_prettifier.cli:main",
+        ],
+    },
+)
---
+++ src/json_prettifier/__init__.py
@@ -0,0 +1,3 @@
+"""json_prettifier - A fast JSON prettifier and minifier."""
+
+__version__ = "1.4.7"
